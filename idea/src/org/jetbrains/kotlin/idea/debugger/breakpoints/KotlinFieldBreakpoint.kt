64c41cfdf2a3c8c6c5debd54c22b622a4f6abe75
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2015-06-08 15:04:20 +0300

    FieldWatchpoints: reload breakpoint type if property changed

--- idea/src/org/jetbrains/kotlin/idea/debugger/breakpoints/KotlinFieldBreakpoint.kt
+++ idea/src/org/jetbrains/kotlin/idea/debugger/breakpoints/KotlinFieldBreakpoint.kt
@@ -48,7 +48,10 @@ import org.jetbrains.kotlin.idea.caches.resolve.analyzeAndGetResult
 import org.jetbrains.kotlin.idea.util.application.runReadAction
 import org.jetbrains.kotlin.load.kotlin.PackageClassUtils
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.JetCallableDeclaration
+import org.jetbrains.kotlin.psi.JetClassOrObject
+import org.jetbrains.kotlin.psi.JetParameter
+import org.jetbrains.kotlin.psi.JetProperty
 import org.jetbrains.kotlin.resolve.BindingContext
 import javax.swing.Icon
 
@@ -66,23 +69,7 @@ class KotlinFieldBreakpoint(
         METHOD
     }
 
-    private val breakpointType: BreakpointType
-
-    init {
-        val element = getField()!!
-        val bindingContext = element.analyzeAndGetResult().bindingContext
-        var descriptor = bindingContext.get(BindingContext.DECLARATION_TO_DESCRIPTOR, element)
-        if (descriptor is ValueParameterDescriptor) {
-            descriptor = bindingContext.get(BindingContext.VALUE_PARAMETER_AS_PROPERTY, descriptor)
-        }
-
-        breakpointType = if (bindingContext.get(BindingContext.BACKING_FIELD_REQUIRED, descriptor as PropertyDescriptor)) {
-            BreakpointType.FIELD
-        }
-        else {
-            BreakpointType.METHOD
-        }
-    }
+    private var breakpointType: BreakpointType = BreakpointType.FIELD
 
     override fun isValid(): Boolean {
         if (!BreakpointWithHighlighter.isPositionValid(getXBreakpoint().getSourcePosition())) return false
@@ -133,6 +120,22 @@ class KotlinFieldBreakpoint(
 
     override fun createRequestForPreparedClass(debugProcess: DebugProcessImpl?, refType: ReferenceType?) {
         if (debugProcess == null || refType == null) return
+
+        val property = getProperty(getSourcePosition())
+        if (property == null) return
+
+        val bindingContext = property.analyzeAndGetResult().bindingContext
+        var descriptor = bindingContext.get(BindingContext.DECLARATION_TO_DESCRIPTOR, property)
+        if (descriptor is ValueParameterDescriptor) {
+            descriptor = bindingContext.get(BindingContext.VALUE_PARAMETER_AS_PROPERTY, descriptor)
+        }
+
+        breakpointType = if (!bindingContext.get(BindingContext.BACKING_FIELD_REQUIRED, descriptor as PropertyDescriptor)) {
+            BreakpointType.METHOD
+        }
+        else {
+            BreakpointType.FIELD
+        }
 
         val vm = debugProcess.getVirtualMachineProxy()
         try {
