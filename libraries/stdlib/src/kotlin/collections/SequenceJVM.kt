54a55b05b62a044ad81dda0ca386a354dc543dd2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-06-15 23:07:55 +0300

    ConstrainedOnceSequence micro-optimization: inherit from AtomicReference instead of including it.

--- libraries/stdlib/src/kotlin/collections/SequenceJVM.kt
+++ libraries/stdlib/src/kotlin/collections/SequenceJVM.kt
@@ -4,11 +4,10 @@ package kotlin
 import java.util.concurrent.atomic.AtomicReference
 
 
-private class ConstrainedOnceSequence<T>(sequence: Sequence<T>) : Sequence<T> {
-    private val sequenceRef = AtomicReference(sequence)
+private class ConstrainedOnceSequence<T>(sequence: Sequence<T>) : Sequence<T>, AtomicReference<Sequence<T>>(sequence) {
 
     override fun iterator(): Iterator<T> {
-        val sequence = sequenceRef.getAndSet(null) ?: throw IllegalStateException("This sequence can be consumed only once.")
+        val sequence = this.getAndSet(null) ?: throw IllegalStateException("This sequence can be consumed only once.")
         return sequence.iterator()
     }
 }
